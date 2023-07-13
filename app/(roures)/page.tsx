9f83df1b9c567ecf3de0147c8dec4f653a73f9ad
Marina Zonova <114925418+marinazonz@users.noreply.github.com>
2023-07-13 17:03:49 +0200

    first commit

---
+++ app/(roures)/page.tsx
@@ -0,0 +1,31 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-product";
+
+import Container from "@/components/ui/conteiner";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+export const HomePage = async () => {
+    const products = await getProducts({ isFeatured: true });
+    const billboard = await getBillboard(
+        "88c0499b-4df1-4749-b5ca-b241f24296cb"
+    );
+
+    return (
+        <Container>
+            <div className='space-y-10 pb-10'>
+                <Billboard data={billboard} />
+            </div>
+            <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8'>
+                <ProductList
+                    title='Featured Products'
+                    items={products}
+                ></ProductList>
+            </div>
+        </Container>
+    );
+};
+
+export default HomePage;
